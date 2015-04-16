b81cdde8f736defa01f1786eea20694ecd1ce3cc
gerardthiers <gerrythiers@gmail.com>
2015-04-16 11:55:17 -0400

    Update testFile.java

--- testFile.java
+++ testFile.java
@@ -1,2 +1,20 @@
 //Hi there!
 //This is a test file
+
+package cctuts;
+
+import javax.swing.*;
+
+public class testInterface extends JFrame{
+	
+	public testInterface(){
+	   setSize(700,700);
+	   JButton startButton = new JButton("Hi!");
+	   setDefaultCloseOperation(EXIT_ON_CLOSE);
+	   setVisible(true);
+	}
+	
+	public static void main(String[] args){ 
+		new testInterface();
+	}
+}
