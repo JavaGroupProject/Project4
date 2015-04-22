d9d039271227e4c1150329d2baf8b123becafea5
gerardthiers <gerrythiers@gmail.com>
2015-04-22 14:29:33 -0400

    prototype of canvas
    UNCOMPLETE primary version of canvas class

---
+++ CanvasPrototype.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Canvas extends JPanel {
+	
+	private MyApplet aApplet;
+	
+	//constructor
+	public Canvas(MyApplet thisApplet) {
+		this.aApplet = thisApplet;
+	}
+	
+	//GETTER methods that depend on quiz info
+	public void getUserInfo() {
+		
+	}
+
+	public void getUserPicture() {
+		
+	}
+	
+	public void getUserFriends() {
+		
+	}
+	
+	public void getUserPrivacy() {
+		
+	}
+	
+	//Display Method
+	//Displays updated profile in accordance with progress through quiz
+	
+}
