08d6c3e8daabde67913e7175bb4a68bf30043a90
zoepark19 <Zoe@Admins-MacBook-Air.local>
2015-04-26 17:40:25 -0400

    user info update

---
+++ UserInfo.java
@@ -0,0 +1,72 @@
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class UserInfo {
+
+	private String name;	
+	private String birthday;
+	private String email;
+	private String address;
+	private String phone;
+	private String ssn;
+	private String profilePicture;
+	
+	public void setName(String aName){
+		name = aName;
+	}
+	
+	public String getName(){
+		return name;
+	}	
+	
+	public void setBirthday(String aBirthday){
+		birthday = aBirthday;
+	}
+	
+	public String getBirthday(){
+		return birthday;
+	}
+	
+	public void setEmail(String anEmail){
+		email = anEmail;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public void setAddress(String anAddress){
+		address = anAddress;
+	}
+	
+	public String getAdress(){
+		return address;
+	}
+	
+	public void setPhone(String aPhone){
+		phone = aPhone;
+	}
+	
+	public String getPhone(){
+		return phone;
+	}
+	
+	public void setSsn(String aSsn){
+		ssn = aSsn;
+	}
+	
+	public String getSsn(){
+		return ssn;
+	}
+	
+	public void setProfilePicture(String aProfilePicture){
+		profilePicture = aProfilePicture;
+	}
+	
+	public String getProfilePicture(){
+		return profilePicture;
+	}
+
+}
